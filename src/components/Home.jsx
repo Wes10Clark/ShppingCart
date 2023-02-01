55ac5d791e1d9f2c2f5a184207f7e5ef84c2b48c
Weston <Wes10clark@gmail.com>
2023-01-31 18:04:56 -0600

    rating

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { CartState } from "../context/Context";
+import SingleProducts from "./SingleProducts";
+import Filters from "./Filters";
+import "../styles.css";
 
 const Home = () => {
-  const { state } = CartState();
+  const {
+    state: { products },
+  } = CartState();
 
-  console.log(state);
-  return <div>Home</div>;
+  console.log(products);
+
+  return (
+    <div className="home">
+      <Filters />
+      <div className="productContainer">
+        {products.map((product) => {
+          return <SingleProducts product={product} key={product.id} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
